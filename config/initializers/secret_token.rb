8592b82229c162775fc48ecde53e8abf8cfde826
corzej <corzej@gmail.com>
2013-11-26 10:07:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Com831oiojin::Application.config.secret_key_base = 'fc5b980cd2bc9c1d909f84247884987314dac19850936f48abdb921161e5c56fb37cb925662fa9eee64f9a83bdd2df63e3d455c60577ec0635accd68f09eec51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Com831oiojin::Application.config.session_store :cookie_store, key: '_com_831oiojin_session'
