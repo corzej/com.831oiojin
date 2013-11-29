95dafd800fdbeb53e5073b6e63ac5a737285ff77
corzej <corzej@gmail.com>
2013-11-30 07:36:17 +0900

    Support I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,11 @@
 Com831oiojin::Application.routes.draw do
-  root "static_pages#home"
-  match '/home', to: 'static_pages#home', via: 'get'
-  match '/work', to: 'static_pages#work', via: 'get'
-  match '/blog', to: 'static_pages#blog', via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    root "static_pages#home"
+    match '/home', to: 'static_pages#home', via: 'get'
+    match '/work', to: 'static_pages#work', via: 'get'
+    match '/blog', to: 'static_pages#blog', via: 'get'
+    match '/contact', to: 'static_pages#contact', via: 'get'
+  end
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), via: 'get'
+  match '', to: redirect("/#{I18n.default_locale}"), via: 'get'
 end
